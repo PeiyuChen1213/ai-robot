911616500738a448478fec055ce21e0ec9cc183e
chenpeiyu <peiyu001@e.ntu.edu.sg>
2025-09-27 02:37:31 +0800

    集成ai

--- src/test/java/com/chenpeiyu/airobot/AiRobotApplicationTests.java
+++ src/test/java/com/chenpeiyu/airobot/AiRobotApplicationTests.java
@@ -1,13 +1,24 @@
 package com.chenpeiyu.airobot;
 
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AiRobotApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Resource
+    private OpenAiChatModel openAiChatModel;
+
+    @Test
+    public void testAliModel(){
+        String call = openAiChatModel.call("你好,请问你是谁");
+        System.out.println(call);
+    }
 
 }
